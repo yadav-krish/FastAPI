cc4075e5ae308b7298a989cf54be43e697471ae6
Krishna <yadavkrish2709@gmail.com>
2025-12-29 21:29:00 +0530

    Learnt Path and HTTP Exception

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path,HTTPException
 import json
 app=FastAPI()
 
@@ -11,7 +11,18 @@ def load_data():
     data=json.load(f)
   
   return data
-    
+
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str = Path(..., description='ID of the patient in the DB', example='P001')):
+    # load all the patients
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail='Patient not found')
+
+
 @app.get('/view')
 def view():
   data=load_data()
